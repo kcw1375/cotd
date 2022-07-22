ede2bd26120ca81c5cb1a6412e7ee6faa3d0a844
kcw <dwong1375@gmail.com>
2022-07-22 04:14:39 -0700

    reads /usr/bin directory, not much else

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // the directory where executables are
+    // ie where $PATH points to
+    let dir = "/usr/bin";
+    
+    // an iterator that contains all files in /usr/bin
+    let mut iter = fs::read_dir(dir).unwrap();
+    println!("{:?}",iter.next());
 }
