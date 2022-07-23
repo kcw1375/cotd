d03c952884cf6ca9c08c312f34025059e5412927
kcw <dwong1375@gmail.com>
2022-07-22 19:23:16 -0700

    now selects a random element from /usr/bin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use rand;
 
 fn main() {
     // the directory where executables are
@@ -6,6 +7,13 @@ fn main() {
     let dir = "/usr/bin";
     
     // an iterator that contains all files in /usr/bin
+    let iter = fs::read_dir(dir).unwrap();
+    let count = iter.count();
+
+    // now choose a random executable
     let mut iter = fs::read_dir(dir).unwrap();
-    println!("{:?}",iter.next());
+    let n : usize = (rand::random::<f32>() * count as f32) as usize;
+    let command = iter.nth(n).unwrap().unwrap();
+
+    println!("{command:?}");
 }
